aa9eff9fe5f594614092d1ebbe6f190a3e9f4a7a
亦梦里 亦书予 <1148161211@qq.com>
2021-07-06 12:00:00 +0800

    a

---
+++ src/test/java/com/example/serch/SerchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
