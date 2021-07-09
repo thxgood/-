67eded5b5df14094be05852f533d9871b792443d
邵凯 <1148161211@qq.com>
2021-07-09 15:28:05 +0800

    创建拿的接口

--- src/main/java/com/example/serch/cotroller/Conntroller.java
+++ src/main/java/com/example/serch/cotroller/Conntroller.java
@@ -27,4 +27,8 @@ public class Conntroller {
     public int in(){
         return inserviceimpl.readJsonFile("北极星电力网.json");
     }
+    @RequestMapping("/take")
+    public  int take(){
+
+    }
 }
